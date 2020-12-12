41a239ba11fcf6a679aee4d5c8e2a73007f462db
achuthofficial <75123755+achuthofficial@users.noreply.github.com>
2020-12-12 16:08:47 +0530

    Add files via upload

---
+++ helloworld2c++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	//printing the multiple lines
+	cout << "first statement";
+	cout << "second statement";
+	//here both the lines are print in a straight line
+	return 0;
+}
---
+++ new line in c++.cpp
@@ -0,0 +1,11 @@
+//using the endl operator to creates a new line
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout << "first statement" << endl;
+	cout << "new line is created second line" << endl;
+	return 0;
+}
